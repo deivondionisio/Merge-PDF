8b3599137a39baa10f252339c20a0a3ca713fead
David Dionísio <49300687+deivondionisio@users.noreply.github.com>
2023-08-26 15:41:22 +0000

    direcionando arquivos a serem mesclados

--- projeto.py
+++ projeto.py
@@ -3,5 +3,11 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
-lista_arquivos = os.listdir()
+lista_arquivos = os.listdir("arquivos")
+lista_arquivos.sort()
 print(lista_arquivos)
+
+for arquivos in lista_arquivos:
+    if ".pdf" in arquivos:
+        merger.append(f"arquivos/{arquivos}")
+merger.write("PDF Final.pdf")
